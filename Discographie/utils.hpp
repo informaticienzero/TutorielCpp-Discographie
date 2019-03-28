28ddae4e9d97548bfdfeca89fd5642a77f640243
informaticienzero <benboss44@hotmail.fr>
2019-03-28 23:16:51 +0100

    Ajout de la documentation pour traitement_chaine.

--- Discographie/utils.hpp
+++ Discographie/utils.hpp
@@ -3,6 +3,13 @@
 
 #include <string>
 
+/**
+* @brief Supprime les espaces et met la chaîne entièrement en minuscule.
+* @details Tous les espaces, avant et après, sont supprimés.
+*
+* @param[in] chaine La chaine à traiter. 
+* @returns Une copie de la chaîne sans espace ni majuscule.
+*/
 std::string traitement_chaine(std::string const & chaine);
 
 #endif
